5d0000aa8e152fd322b7f9bd36bd48e9ea6e2e3e
Ishan Satyanand Thakur <ishanst.shares@gmail.com>
2025-08-29 13:33:00 +0530

    Create 4.py

---
+++ LAB2/4.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+
+key=b"A1B2C3D4"
+iv=b"12345678"
+msg=b"Secure Communication"
+cipher=DES.new(key,DES.MODE_CBC,iv)
+ct=cipher.encrypt(pad(msg,8))
+print("Ciphertext:",ct.hex())
+pt=unpad(DES.new(key,DES.MODE_CBC,iv).decrypt(ct),8)
+print("Decrypted:",pt.decode())
