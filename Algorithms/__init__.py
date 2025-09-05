f9795466ed9c4777c3dc641e8e24c479d221d0e7
Ishan Satyanand Thakur <ishanst.shares@gmail.com>
2025-09-05 15:46:41 +0530

    Update __init__.py

--- Algorithms/__init__.py
+++ Algorithms/__init__.py
@@ -1,4 +1,4 @@
-from .aes import aes
-from .des import des
+from .aes_128 import aes_128
+from .des_s import des_s
 from .modularinverse import modularinverse
 from .rsa import rsa_encrypt, rsa_decrypt
