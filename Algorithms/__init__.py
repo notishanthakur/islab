6d617cc09139db6485ccbc052eacd605b9c026da
Ishan Satyanand Thakur <ishanst.shares@gmail.com>
2025-09-26 13:35:35 +0530

    Update __init__.py

--- Algorithms/__init__.py
+++ Algorithms/__init__.py
@@ -2,7 +2,9 @@ from .aes_128 import aes_128
 from .des_s import des_s
 from .modularinverse import modularinverse
 from .rsa import rsa_encrypt, rsa_decrypt
-
+from .ecc import ecc_encrypt, ecc_decrypt, ecc_keygen
+from .elgamal import elgamal_encrypt, elgamal_decrypt, elgamal_keygen
+from .dh import dh_keygen, dh_shared_secret
 #IMPLEMENTATION DUE
 #from .ecc import ecc_encrypt, ecc_decrypt
 #from .elGamal import elGamal_encrypt, elGamal_decrypt
